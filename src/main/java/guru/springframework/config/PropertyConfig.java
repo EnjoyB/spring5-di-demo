200a2832b1dae244a150af7e93345e346607f766
Daniel Šulik <sulikdan@fit.cvut.cz>
2020-02-07 21:55:03 +0100

    Using build enviroment properties instead of properties specified in resources... #095

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 /**
  * Created by Daniel Šulik on 07-Feb-20
@@ -15,6 +17,9 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment environment;
 
     @Value("${demo.username}")
     String user;
@@ -28,7 +33,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource dataSource = new FakeDataSource();
-        dataSource.setUser(user);
+        dataSource.setUser(environment.getProperty("username"));
         dataSource.setPassword(password);
         dataSource.setUrl(url);
         return dataSource;
